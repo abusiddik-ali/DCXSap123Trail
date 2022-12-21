f1c44d627b8701be867631076c2090b7071adc3f
Shaikh <abusiddik-ali.shaikh@capgemini.com>
2022-12-21 19:20:23 +0530

    Test Your Service

---
+++ bin/custom/concerttours/src/concerttours/daos/impl/DefaultBandDAO.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package concerttours.daos.impl;
+
+import de.hybris.platform.servicelayer.search.FlexibleSearchQuery;
+import de.hybris.platform.servicelayer.search.FlexibleSearchService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import concerttours.daos.BandDAO;
+import concerttours.model.BandModel;
+
+
+/**
+ *
+ */
+@Component(value = "BandDAO")
+public class DefaultBandDAO implements BandDAO
+{
+	/**
+	 * Use SAP Commerce FlexibleSearchService for running queries against the database
+	 */
+	@Autowired
+	private FlexibleSearchService flexibleSearchService;
+
+	@Override
+	public List<BandModel> findBands()
+	{
+		//Build a query for the flexible search.
+		final String queryString = "Select {p:" + BandModel.PK + "}" + " From {" + BandModel._TYPECODE + " AS p}";
+		final FlexibleSearchQuery query = new FlexibleSearchQuery(queryString);
+
+		// Note that we could specify paginating logic by providing a start and count variable (commented out below)
+		// This can provide a safeguard against returning very large amounts of data, or hogging the database when there are
+		// for example millions of items being returned.
+		// As we know that there are only a few persisted bands in this use case we do not need to provide this.
+		//query.setStart(start);
+		//query.setCount(count);
+		// Return the list of BandModels.
+		return flexibleSearchService.<BandModel> search(query).getResult();
+	}
+
+	/**
+	 * Finds all Bands by given code by performing a FlexibleSearch using the {@link FlexibleSearchService}.
+	 */
+	@Override
+   public List<BandModel> findBandsByCode(final String code)
+	{
+		final String queryString = "Select {p:" + BandModel.PK + "}" 
+				+ " From {" + BandModel._TYPECODE + " AS p}" 
+				+ "WHERE {p:" + BandModel.CODE + "}=?code";
+
+		final FlexibleSearchQuery query = new FlexibleSearchQuery(queryString);
+      query.addQueryParameter("code", code);
+      return flexibleSearchService.<BandModel> search(query).getResult();
+
+	}
+}
--- bin/platform/bootstrap/gensrc/de/hybris/platform/processengine/adminapi/TriggerEventRequest.java
+++ bin/platform/bootstrap/gensrc/de/hybris/platform/processengine/adminapi/TriggerEventRequest.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN!
- * --- Generated at 16-Dec-2022, 6:42:55 PM
+ * --- Generated at 21-Dec-2022, 4:02:24 PM
  * ----------------------------------------------------------------
  *
  * Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved.
